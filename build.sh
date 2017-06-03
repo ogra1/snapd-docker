49847e318f9a7b766cbbe34c1d3aedaa238f599e
Oliver Grawert <ogra@ubuntu.com>
2017-06-03 12:27:13 +0200

    rework Dockerfile

--- build.sh
+++ build.sh
@@ -59,10 +59,16 @@ if [ -z "$($SUDO docker images|grep $IMGNAME)" ]; then
     cat << EOF > $BUILDDIR/Dockerfile
 FROM ubuntu:$RELEASE
 ENV container docker
-RUN apt-get update
-RUN apt-get install -y fuse snapd snap-confine squashfuse
-RUN rm -f /sbin/udevadm; ln -s /bin/true /sbin/udevadm
-RUN mkdir -p /lib/modules
+ENV PATH "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"
+ENV LANG C.UTF-8
+ENV LC_ALL C.UTF-8
+RUN apt-get update &&\
+ DEBIAN_FRONTEND=noninteractive\
+ apt-get install -y fuse snapd snap-confine squashfuse sudo &&\
+ dpkg-divert --local --rename --add /sbin/udevadm &&\
+ apt-get clean &&\
+ ln -s /bin/true /sbin/udevadm &&\
+ mkdir -p /lib/modules
 RUN systemctl enable snapd
 VOLUME [ “/sys/fs/cgroup” ]
 STOPSIGNAL SIGRTMIN+3
